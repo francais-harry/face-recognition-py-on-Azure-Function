a15da8e3ee53f09369924188f3288686c1f60a93
Hisato Shoji <francais.harry@gmail.com>
2023-12-28 17:14:37 +0900

    First version of Azure Function to recognize face

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import recognition
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,19 +8,32 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    input_url = req.params.get('input_url')
+    if not input_url:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            input_url = req_body.get('input_url')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    httpResponse = None
+
+    if input_url:
+        logging.info('Got request with url= ' + input_url)
+        try:
+            result = recognition.find(input_url)
+
+            if result is not None:
+                httpResponse = func.HttpResponse(f"{result}")
+        except Exception as e:
+            logging.warning(f"Some error happens with {e.args}")
+            httpResponse = func.HttpResponse(f"Something bad happens with {input_url}. message: {e.args}", status_code=500)
+
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        httpResponse = func.HttpResponse("Invalid request parameter", status_code=400)
+    
+    if httpResponse is None:
+        httpResponse = func.HttpResponse(f"Not detected")
+
+    return httpResponse
---
+++ recognition.py
@@ -0,0 +1,73 @@
+import face_recognition
+import io
+import requests
+import os
+import statistics
+
+def load_image(url):
+    online_file = io.BytesIO(requests.get(url).content)
+    return face_recognition.load_image_file(online_file)
+
+def load_single_encoding_from_url(url):
+    image = load_image(url)
+    face_locations = face_recognition.face_locations(image)
+
+    if len(face_locations) == 1:
+        # set large jitters to get accurate encodings as source data
+        encoding = face_recognition.face_encodings(image, known_face_locations=face_locations, num_jitters=50)[0]
+        print(f'encoding= {encoding}')
+        return encoding
+    else:
+        return None
+
+def get_encodings(file_name):
+    print(f'loading file= {file_name}')
+    encodings = []
+
+    with open(file_name, 'r') as file:
+
+        for line in file:
+            print(f'loading url= {line}')
+            encoding = load_single_encoding_from_url(line)
+            if encoding is not None:
+                encodings.append(encoding)
+
+    return encodings
+
+def get_encoding_set():
+    encoding_set = {}
+
+    files = os.listdir('source_data/')
+
+    # make key value pair of source data file name and encoding value
+    # single file can contain multiple URLs as source data
+    for file in files:
+        encoding_set[file] = get_encodings('source_data/' + file)
+
+    return encoding_set
+
+
+def find(url, tolerance=0.35):
+    encoding_set = get_encoding_set()
+
+    test_image = load_image(url)
+    face_locations = face_recognition.face_locations(test_image)
+    test_encodings = face_recognition.face_encodings(test_image, known_face_locations=face_locations, model="large")
+
+    retVal = None
+
+    for x in range(len(face_locations)):
+        test_encoding = test_encodings[x]
+        print(f'face location= {face_locations[x]}')
+
+        for encoding_key in encoding_set:
+            results = face_recognition.face_distance(encoding_set[encoding_key], test_encoding)
+            mean = statistics.mean(results)
+
+            print(f'key={encoding_key} mean={mean}')
+
+            if mean < tolerance:
+                retVal = encoding_key
+
+    return retVal
+
